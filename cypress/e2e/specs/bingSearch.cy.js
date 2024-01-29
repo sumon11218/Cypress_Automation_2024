34ed6897311ea8a7d8da9af6f386d9d067f1c560
triage11218 <triageconsulting.ny@gmail.com>
2024-01-29 16:14:14 -0500

    new changes

--- cypress/e2e/specs/bingSearch.cy.js
+++ cypress/e2e/specs/bingSearch.cy.js
@@ -1,16 +1,19 @@
 
 it('Navigates to bing search and search for a AUDI car and then get the search number from result page',() =>{
+    console.log("Navigate to bing home page")
     cy.visit('https://www.bing.com');
-    cy.xpath('//*[@name="q"]',{timeout:7000}).type("AUDI");
-    cy.wait(2800)
+    console.log("Enter keyword 'audi search' on the search field")
+    cy.xpath('//*[@name="q"]',{timeout:7000}).type("audi search");
+    console.log("Click on search icon")
     cy.xpath('//*[@id="search_icon"]',{timeout:7000}).click({force:true}); 
+    console.log("Capture search results for Audi")
     cy.xpath('//div[@class="sb_count"]',{timeout:7000}).then(($searchResult) => {
         const text = $searchResult.text();
         console.log("Search result for AUDI is " + text)
         cy.log("Search result for AUDI is " + text);
         const arrayResult = text.split(' ');
         cy.log("Search number for AUDI is " + arrayResult[1]);
-        console.log("Search number for AUDI is " + arrayResult[1])
+        console.log("Search number for Audi is " + arrayResult[1])
     })//end of then
 })//end of it test
 
